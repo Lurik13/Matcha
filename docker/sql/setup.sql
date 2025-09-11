54ca60e5497330ca0c11cedfda0175774685d15a
Berengere76 <berengerelebas@hotmail.com>
2025-09-11 15:24:13 +0200

    started to create table

--- docker/sql/setup.sql
+++ docker/sql/setup.sql
@@ -1,5 +1,10 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
 
 );
 
