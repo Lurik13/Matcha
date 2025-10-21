bab1c0afec8dd00ea779c34f87b073f8b296cea7
Lurik13 <lucasribette.pro@gmail.com>
2025-10-21 20:06:18 +0200

    Première version fonctionnelle des étoiles

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import { Stage, Layer, Circle, Rect } from 'react-konva';
+import { Stage, Layer } from 'react-konva';
 import Konva from 'konva';
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Input from '$/components/Input';
 
 const WIDTH = window.innerWidth
@@ -8,28 +8,35 @@ const HEIGHT = window.innerHeight
 
 const Canvas = () => {
   const layerRef = useRef<any>(null)
-
+  layerRef.current?.add(new Konva.Rect({
+    width:window.innerWidth,
+    height:window.innerHeight,
+    fill:"black"
+  }));
   useEffect(() => {
-    for (let i = 0; i < 80; i++) {
-      const radius = 5 + Math.random() * 15
+    for (let i = 0; i < 1000; i++) {
+      const radius = Math.random()
       const circle = new Konva.Circle({
         x: Math.random() * WIDTH,
         y: Math.random() * HEIGHT,
         radius,
-        fill: Konva.Util.getRandomColor(),
-        opacity: 0.4,
+        fill: 'black',
       })
       layerRef.current?.add(circle)
     }
 
     const anim = new Konva.Animation(() => {
-      layerRef.current?.children?.forEach((node: any) => {
-        const r = node.radius()
-        const speed = 0.2 + r * 0.03
-        node.x(node.x() - speed)
-        node.y(node.y() + speed * 0.3)
-        if (node.x() < -r) node.x(WIDTH + r)
-        if (node.y() > HEIGHT + r) node.y(-r)
+      layerRef.current?.children?.forEach((star: any) => {
+        const star_radius = star.radius()
+        const speed = star_radius
+        star.x(star.x() - speed)
+        star.y(star.y() + speed * 0.3)
+        if (star.x() < -star_radius) {
+          star.x(WIDTH + star_radius);
+        }
+        if (star.y() > HEIGHT + star_radius) {
+          star.y(-star_radius);
+        }
       })
     }, layerRef.current)
 
