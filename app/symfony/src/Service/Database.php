3738a3b4432b2baaec2fd83b459e0c4eb9fcd441
Bérengère <berengerelebas@hotmail.com>
2025-10-23 16:02:09 +0200

    create connexion db

---
+++ app/symfony/src/Service/Database.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Service;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private $pdo;
+
+    public function __construct(string $db_host, string $db_name, string $db_user, string $db_password)
+    {
+        try {
+            $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
+            
+            $this->pdo = new PDO($dsn, $db_user, $db_password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->pdo;
+    }
+}
