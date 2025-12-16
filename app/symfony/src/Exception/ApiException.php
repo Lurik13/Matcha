3e9804423bebd66fcad66fedf6b17ea3799b9bb1
Bérengère <berengerelebas@hotmail.com>
2025-12-16 10:00:50 +0100

    add api exception for error logic

---
+++ app/symfony/src/Exception/ApiException.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Exception;
+
+use Exception;
+
+class ApiException extends Exception
+{
+
+    private string $name;
+    private int $status;
+
+
+    public function __construct(string $name, string $message, int $status = 400)
+    {
+        parent::__construct($message);
+        $this->name = $name;
+        $this->status = $status;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+}
