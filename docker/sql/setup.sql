638a7857f34624dc0f4ac5829e95b7d2e75857eb
Bérengère <berengerelebas@hotmail.com>
2025-12-17 15:02:49 +0100

    modified users table

--- docker/sql/setup.sql
+++ docker/sql/setup.sql
@@ -4,5 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL,
     username VARCHAR(50) NOT NULL UNIQUE,
     firstname VARCHAR(50) NOT NULL,
-    lastname VARCHAR(50) NOT NULL
+    lastname VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
